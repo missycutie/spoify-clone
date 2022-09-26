10a623a9a56fc5788b299bcaf2a1df2882a4e49c
Chau Trinh <trinhchau.1006@gmail.com>
2022-09-26 17:54:47 +0700

    done

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import loader from "./loader.svg";
+import logo from "./logo.svg";
+import music from "./music.png";
+import guitar from "./guitar.png";
+import woman from "./woman.png";
+
+export { logo, loader, music, guitar, woman };
